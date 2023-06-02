df0b64767d114b400713abdfddab279f41b2824b
Matt <changan.cs10@nycu.edu.tw>
2023-06-02 17:06:27 +0800

    add month selection

--- index.php
+++ index.php
@@ -1,15 +1,52 @@
 <!DOCTYPE html>
 <html lang="zh-TW">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>用電資料視覺化 :: 資料庫 期末專案</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="/assets/css/main.css">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>用電資料視覺化 :: 資料庫 期末專案</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="/assets/css/main.css">
 </head>
 <body>
 	<div class="container">
 		<form id="query">
 			<input type="submit">
+
+			<h2>電量視覺化</h2>
+
+			<select>
+				<option>選擇一個縣市</option>
+				<option></option>
+				<option></option>
+			</select>
+
+			<select>
+				<option>選擇一個年分</option>
+				<option>109</option>
+				<option>108</option>
+				<option>107</option>
+				<option>106</option>
+				<option>105</option>
+				<option>104</option>
+				<option>103</option>
+				<option>102</option>
+			</select>
+
+			<select>
+				<option>選擇一個月份</option>
+				<option>1</option>
+				<option>2</option>
+				<option>3</option>
+				<option>4</option>
+				<option>5</option>
+				<option>6</option>
+				<option>7</option>
+				<option>8</option>
+				<option>9</option>
+				<option>10</option>
+				<option>11</option>
+				<option>12</option>
+			</select>
+
 		</form>
 	</div>
 	<script src="assets/js/main.js"></script>
