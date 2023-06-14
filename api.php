c8d882b64a434fd2e5bec88e24b4d4ba77921b16
許家維 <ric12345wei@gmail.com>
2023-06-14 21:30:46 +0800

    api change in pdo form

--- api.php
+++ api.php
@@ -14,54 +14,94 @@ $result = [
 
 header('Content-Type: application/json');
 
-$year = $_POST['year'] ;
-$month = $_POST['month'] ;
+$year = intval($_POST['year']) ;
+$month = intval($_POST['month'] );
 $city = $_POST['city'] ;
-$dist = $_POST['dist'] ;
-/*
-$district = $_POST['district'] ;
-$district_int = intval($_POST['district']);
- */
-$date = $year . '-' . $month . '-01';
-$formatDate = date('Y-m', strtotime($date));
-
-$schema = array('electricity_by_village', 'zip_map', 'past_5_year', '2_month_prediction', 'industry_per');
+$district = $_POST['dist'] ;
+$choice = $_POST['choice'] ;
 
 switch($choice){
 	case 1:
-		$table_name = "electricity_by_village";
+		$table_name = "by_type";
 		break;
 	case 2:
-		$table_name = "industry_per";
+		$table_name = "by_industry";
 		break;
 	case 3:
-		$table_name = "2_month_prediction";
+		$table_name = "over_view";
 		break;
 	case 4:
-		$table_name = "past_5_year";
+		$table_name = "prediction";
 		break;
 	default:
-		$table_name = "electricity_by_village";
+		$table_name = "by_type";
 		break;
 }
 
 
 
-if($year != "" && $month != "" && $table_name == "electricity_by_village")
-	$sqlQuery = "SELECT * FROM electricity_by_village JOIN zip_map ON electricity_by_village.zip_code = zip_map.zip_code WHERE `year` = $year AND `month` = $month";
-else if($year != "" && $month != "")
-	$sqlQuery = "SELECT * FROM $table_name WHERE `year` = $year AND `month` = $month AND 'city' = $city AND 'district' = $district";
-else
-	$sqlQuery = "SELECT * FROM $table_name WHERE `year` = 110 AND `month` = 1"; // default?
+if($year != "" && $month != "" && $dist != "" && $table_name == "by_type"){
+	$sql = "SELECT * FROM `by_type` JOIN `zip_map`
+	ON `by_type.zip_code = zip_map.zip_code` WHERE `year` = ':year' AND `month` = ':month' AND `dist` = ':dist'";
+	$stmt = $pdo->prepare($sql);
+    $stmt->execute([
+        'year' => $year,
+        'month' => $month,
+        'dist' => $dist,
+    ]);
+	$results = [];
+    while ($item = $stmt->fetch()) {
+	    $results[] = $item;
+    }
+}
+else if($year != "" && $month != "" && $city != "" && $table_name == "by_industry"){
+	$sql = "SELECT * FROM `by_industry` WHERE `year` = ':year' AND `month` = ':mont'h AND 'city' = ':city'";
+	$stmt = $pdo->prepare($sql);
+    $stmt->execute([
+        'year' => $year,
+        'month' => $month,
+        'city' => $city,
+    ]);
+	$results = [];
+    while ($item = $stmt->fetch()) {
+	    $results[] = $item;
+    }
+}
+else if($year != "" && $table_name == "overview"){
+	$sql = "SELECT * FROM `overview` WHERE `year` = ':year'";
+	$stmt = $pdo->prepare($sql);
+    $stmt->execute([
+        'year' => $year,
+    ]);
+	$results = [];
+    while ($item = $stmt->fetch()) {
+	    $results[] = $item;
+    }
+}
+else if($year != "" && $month != "" && $table_name == "prediction"){
+	$sql = "SELECT * FROM `prediction` WHERE `year` = ':year' AND `month` = ':month'";
+	$stmt = $pdo->prepare($sql);
+    $stmt->execute([
+        'year' => $year,
+        'month' => $month,
+    ]);
+	$results = [];
+    while ($item = $stmt->fetch()) {
+	    $results[] = $item;
+    }
+}
+else{
+	$sql = "SELECT * FROM `prediction` WHERE `year` = 2020 AND `month` = 1"; // default?
 
-$stmt = $pdo->prepare($sql);
-$stmt->execute([
-	'year' => $year,
-]);
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([
+	    'year' => $year,
+    ]);
 
-$results = [];
-while ($item = $stmt->fetch()) {
-	$results[] = $item;
+    $results = [];
+    while ($item = $stmt->fetch()) {
+	    $results[] = $item;
+    }
 }
 
 echo json_encode([
