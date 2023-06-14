1c0ababd52274a4a3c0cb889d66b9199a484fe3b
許家維 <ric12345wei@gmail.com>
2023-06-14 18:16:32 +0800

    Adding name tag in index and api sql

--- api.php
+++ api.php
@@ -10,41 +10,57 @@ header('Content-Type: application/json');
 
 $year = $_POST['year'] ;
 $month = $_POST['month'] ;
+$year_int = intval($_POST['year']);
+$month_int = intval($_POST['month']);
 $city = $_POST['city'] ;
-$district = $_POST['dist'] ;
-$choice = $_POST['choice'] ;
+$dist = $_POST['dist'] ;
+/*
+$district = $_POST['district'] ;
+$district_int = intval($_POST['district']);
+ */
+$date = $year . '-' . $month . '-01';
+$formatDate = date('Y-m', strtotime($date));
 
-switch($choice){
-	case 1:
-		$table_name = "electricity_by_village";
-		break;
-	case 2:
-		$table_name = "industry_per";
-		break;
-	case 3:
-		$table_name = "2_month_prediction";
-		break;
-	case 4:
-		$table_name = "past_5_year";
-		break;
-	default:
-		$table_name = "electricity_by_village";
-		break;
+$schema = array('electricity_by_village', 'zip_map', 'past_5_year', '2_month_prediction', 'industry_per');
+
+foreach($schema as $value){
+    switch($value){
+		case 'electricity_by_village';
+		    if($year != "" && $month != "" && $district != "")
+		        $sqlQuery = "SELECT * FROM `electricity_by_village` JOIN `zip_map` ON `zip_map.zip_code` = `electricity_by_village.zip_code`
+				WHERE `year` = ($year_int - 1911) AND `month` = $month_int AND `zip_map.dist` = $dist";
+	        else
+		        $sqlQuery = "SELECT * FROM $value WHERE `year` = 110 AND `month` = 1 AND `zip_code` = '中正區'";
+			break;
+		case 'zip_map';
+		    if($dist != "")
+		        $sqlQuery = "SELECT * FROM `zip_map` WHERE `zip_dist` = $dist";
+	        else
+		        $sqlQuery = "SELECT * FROM `zip_map` WHERE `zip_dist` = '中正區'";
+		    break;
+		case 'past_5_year';
+		    if($year != "")
+		        $sqlQuery = "SELECT * FROM `past_5_year` WHERE `year` = $year_int";
+	        else
+		        $sqlQuery = "SELECT * FROM `past_5_year` WHERE `year` = 2021";
+			break;
+		case '2_month_prediction';
+		    if($year != "" && $month != "")
+		        $sqlQuery = "SELECT * FROM `2_month_prediction` WHERE DATE_FORMAT(`date`, '%Y-%m') = $formatDate";
+	        else
+		        $sqlQuery = "SELECT * FROM `2_month_prediction` WHERE DATE_FORMAT(`date`, '%Y-%m') = 2021-01";
+			break;	
+		case 'industry_per';
+		    if($year != "" && $month != "" && $city != "")
+		        $sqlQuery = "SELECT * FROM `industry_per` WHERE `city` = $city AND DATE_FORMAT(`industry_per.date`, '%Y-%m') = $formatDate";
+	        else
+			    $sqlQuery = "SELECT * FROM `industry_per` WHERE `city` = '台北市' AND DATE_FORMAT(`industry_per.date`, '%Y-%m') = 2021-01";
+			break;
+	}
+	echo json_encode([
+		'ok' => true,
+		'result' => [
+			'sqlQuery' => $sqlQuery,
+		]
+	], JSON_PRETTY_PRINT);
 }
-
-
-
-if($year != "" && $month != "" && $table_name == "electricity_by_village")
-	$sqlQuery = "SELECT * FROM electricity_by_village JOIN zip_map ON electricity_by_village.zip_code = zip_map.zip_code WHERE `year` = $year AND `month` = $month";
-else if($year != "" && $month != "")
-	$sqlQuery = "SELECT * FROM $table_name WHERE `year` = $year AND `month` = $month AND 'city' = $city AND 'district' = $district";
-else
-	$sqlQuery = "SELECT * FROM $table_name WHERE `year` = 110 AND `month` = 1"; // default?
-
-
-echo json_encode([
-	'ok' => true,
-	'result' => [
-		'sqlQuery' => $sqlQuery,
-	]
-], JSON_PRETTY_PRINT);
